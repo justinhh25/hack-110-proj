76dcf86a24825664f93cdb60110e5cde0e4f0d6b
dhruvnpUNC <dhruvnp@ad.unc.edu>
2024-11-09 11:38:20 -0500

    Co-authored-by: Justin H <justinhh25@users.noreply.github.com>

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Topbar from './components/topbar';
 import './App.css'
 
 function App() {
   return (
     <>
+      <Topbar/>
     </>
   );
 }
---
+++ my-react-app/src/components/Topbar.jsx
@@ -0,0 +1,15 @@
+import "./Topbar.css"
+
+function Topbar() {
+    return(
+        <>
+            <div className="topbar">
+                <h2>Home</h2>
+                <h2>Find</h2>
+                <h2>Liked</h2>
+            </div>
+        </>
+    );
+}
+
+export default Topbar
